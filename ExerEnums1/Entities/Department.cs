b450e4a4151e2ed4aeba77a906e10ba455599ffb
Neemias Pereira <neemias.pereira3@gmail.com>
2022-02-01 20:27:22 -0300

    Programa para verificar ganhos baseado nos contratos feitos

---
+++ ExerEnums1/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExerEnums1.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() 
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
